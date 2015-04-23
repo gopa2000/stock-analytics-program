c1ed3b57cd16eeaa9766cc123dcaf5190efc9d71
gopa2000 <doberoi96@gmail.com>
2015-04-24 04:43:11 +0800

    "reduced to just display; calculation done in getStockInfo.m"

--- displayStockInfo.m
+++ displayStockInfo.m
@@ -1,25 +1,12 @@
 function [] = displayStockInfo(stock)
-    % Calculated Stock Information according to stock prices from 1/4/14 -
-    % 1/4/15
-    
-    %% Still gotta figure this out.
-    %% Name = varname('stock');
-    TradingDays = length(stock);
-    AnnualReturn = ((stock(end).AdjClose/stock(1).AdjClose)-1)*100;
-    AvgDailyReturn = ((sum([stock.DailyReturns]))/TradingDays)*100;
-    AvgAnnualisedReturn = (AvgDailyReturn*TradingDays);
-    DailyVolatility = (std([stock.DailyReturns]))*100;
-    AnnualisedVolatility = (sqrt(TradingDays)*DailyVolatility);
-    SharpeRatio = ((AvgAnnualisedReturn)-0.005)/AnnualisedVolatility;
-    
     clc;
     %% fprintf('Name:\t%s\n',Name);
-    fprintf('Annual return:\t%.2f\n',AnnualReturn);
-    fprintf('Average Daily Return:\t%.2f\n', AvgDailyReturn);
-    fprintf('Average Annualised Return:\t%.2f\n',AvgAnnualisedReturn);
-    fprintf('Daily Volatility:\t%.2f\n',DailyVolatility);
-    fprintf('Annualised Volatility:\t%.2f\n', AnnualisedVolatility);
-    fprintf('Sharpe Ratio:\t%.2f\n', SharpeRatio);
+    fprintf('Annual return:\t%.2f\n',stock.AnnualReturn);
+    fprintf('Average Daily Return:\t%.2f\n', stock.AvgDailyReturn);
+    fprintf('Average Annualised Return:\t%.2f\n',stock.AvgAnnualisedReturn);
+    fprintf('Daily Volatility:\t%.2f\n',stock.DailyVolatility);
+    fprintf('Annualised Volatility:\t%.2f\n', stock.AnnualisedVolatility);
+    fprintf('Sharpe Ratio:\t%.2f\n', stock.SharpeRatio);
     
     %% I'm using pause since waitforbuttonpress opens an unwanted figure.
     fprintf('Press any key to continue.');
