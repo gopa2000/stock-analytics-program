ce030ab7b23127173b5195547306bf50b54eb67e
gopa2000 <doberoi96@gmail.com>
2015-04-24 04:43:58 +0800

    "finds stocks info and returns to Info branch of stock structure"

---
+++ getStockInfo.m
@@ -0,0 +1,13 @@
+function calData = getStockInfo(stock)
+    % Calculated Stock Information according to stock prices from 1/4/14 -
+    % 1/4/15 
+    TradingDays = length(stock);
+    AnnualReturn = ((stock(end).AdjClose/stock(1).AdjClose)-1)*100;
+    AvgDailyReturn = ((sum([stock.DailyReturns]))/TradingDays)*100;
+    AvgAnnualisedReturn = (AvgDailyReturn*TradingDays);
+    DailyVolatility = (std([stock.DailyReturns]))*100;
+    AnnualisedVolatility = (sqrt(TradingDays)*DailyVolatility);
+    SharpeRatio = ((AvgAnnualisedReturn)-0.005)/AnnualisedVolatility;
+    
+    calData = struct('TradingDays', TradingDays, 'AnnualReturn', AnnualReturn, 'AvgDailyReturn', AvgDailyReturn, 'AvgAnnualisedReturn', AvgAnnualisedReturn, 'DailyVolatility', DailyVolatility, 'AnnualisedVolatility', AnnualisedVolatility, 'SharpeRatio', SharpeRatio);
+end
