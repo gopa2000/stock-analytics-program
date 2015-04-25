b6086ae758db4fd2f0a9f3fc0fbad2ff8c8ee368
gopa2000 <doberoi96@gmail.com>
2015-04-26 00:39:32 +0800

    "-"

--- displayStockInfo.m
+++ displayStockInfo.m
@@ -1,15 +1,10 @@
 function [] = displayStockInfo(stock)
     clc;
-    %% fprintf('Name:\t%s\n',Name);
-    fprintf('Annual return:\t%.2f\n',stock.AnnualReturn);
-    fprintf('Average Daily Return:\t%.2f\n', stock.AvgDailyReturn);
-    fprintf('Average Annualised Return:\t%.2f\n',stock.AvgAnnualisedReturn);
-    fprintf('Daily Volatility:\t%.2f\n',stock.DailyVolatility);
-    fprintf('Annualised Volatility:\t%.2f\n', stock.AnnualisedVolatility);
-    fprintf('Sharpe Ratio:\t%.2f\n', stock.SharpeRatio);
+    %% Displays Stock Info
+    stock    
     
     %% I'm using pause since waitforbuttonpress opens an unwanted figure.
-    fprintf('Press any key to continue.');
+    fprintf('Press any key to continue.\n');
     pause('on');
     pause;
 end
